d5388745ced0d55b2e53982c03a77a1730665754
ajay16108 <65584647+ajay16108@users.noreply.github.com>
2021-02-22 14:56:47 +0530

    Update file.c
    first

--- file.c
+++ file.c
@@ -3,7 +3,7 @@
 main() {
    FILE *fp;
 
-   fp = fopen("/tmp/test.txt", "w+");
+   fp = fopen("test.txt", "w+");
    fprintf(fp, "This is testing for fprintf...\n");
    fputs("This is testing for fputs...\n", fp);
    fclose(fp);
