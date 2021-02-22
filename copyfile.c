3d617ea9e1a0e825a077cdc21fad6ffe4f79cbca
ajay16108 <65584647+ajay16108@users.noreply.github.com>
2021-02-22 17:22:38 +0530

    Update copyfile.c
    ajay

--- copyfile.c
+++ copyfile.c
@@ -4,7 +4,7 @@ int main()
   int ch;
   FILE *fp,*fp2;
   fp=fopen("readfile.c","r");
-  fp2=fopen("ajay3.txt","w");
+  fp2=fopen("aj.txt","w");
   while((ch=fgetc(fp))!=EOF)
   {
     fputc(ch,fp2);
