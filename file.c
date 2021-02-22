a721e4fefe2525398d03e0c2167bff0b7b5a137e
ajay16108 <65584647+ajay16108@users.noreply.github.com>
2021-02-22 15:01:42 +0530

    Update file.c
    ajua

--- file.c
+++ file.c
@@ -3,7 +3,7 @@
 main() {
    FILE *fp;
 
-   fp = fopen("test1.txt", "w+");
+   fp = fopen("test2.txt", "w+");
    fprintf(fp, "This is testing for fprintf...\n");
    fputs("This is testing for fputs...\n", fp);
    fclose(fp);
