931bdcf20639a115085ed90be6da58eacfaf33e7
ajay16108 <65584647+ajay16108@users.noreply.github.com>
2021-02-22 14:59:52 +0530

    Update file.c
    ajay

--- file.c
+++ file.c
@@ -3,7 +3,7 @@
 main() {
    FILE *fp;
 
-   fp = fopen("test.txt", "w+");
+   fp = fopen("test1.txt", "w+");
    fprintf(fp, "This is testing for fprintf...\n");
    fputs("This is testing for fputs...\n", fp);
    fclose(fp);
