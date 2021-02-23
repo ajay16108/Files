252dde74a6aa223ba8c22efaceeb370669e032cc
ajay16108 <65584647+ajay16108@users.noreply.github.com>
2021-02-23 12:58:33 +0530

    Create seekread.c
    sdswd

---
+++ seekread.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+struct node
+{
+    int data;
+    struct node *link;
+};
+typedef struct node ll;
+int main()
+{
+    ll a2;
+    FILE *fp;
+    fp=fopen("data","rb");
+    fseek(fp,(sizeof(a2)),SEEK_SET);
+    fread(&a2,(sizeof(a2)),1,fp);
+    printf("reading done");
+    printf("%d %d",a2.data,a2.link);
+    fclose(fp);
+}
