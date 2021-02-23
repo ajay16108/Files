67ba3c351ea6a13ed7369ab20a9df3caea673f47
ajay16108 <65584647+ajay16108@users.noreply.github.com>
2021-02-23 17:57:44 +0530

    Update pipe.c
    dfdf

--- pipe.c
+++ pipe.c
@@ -3,7 +3,8 @@
 char*  msg1 = "hello, world #1"; 
 char* msg2 = "hello, world #2"; 
 char* msg3 = "hello, world #3"; 
-int a=10,b=20,c=30;
+char* msg4 = "hello, world #4"; 
+
   
 int main() 
 { 
@@ -20,9 +21,10 @@ int main()
     write(p[1], msg1, 16); 
     write(p[1], msg2, 16); 
     write(p[1], msg3, 16);
+   write(p[1], msg4, 16);
     
   
-    for (i = 0; i < 3; i++) { 
+    for (i = 0; i < 4; i++) { 
         /* read pipe */
         read(p[0], inbuf, 16); 
         printf("% s\n", inbuf); 
