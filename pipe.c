2d05fb466cbd4c3fe8a9d797f54b19b418d0172a
ajay16108 <65584647+ajay16108@users.noreply.github.com>
2021-02-23 18:03:49 +0530

    Update pipe.c
    wdew

--- pipe.c
+++ pipe.c
@@ -4,6 +4,7 @@ char*  msg1 = "hello, world #1";
 char* msg2 = "hello, world #2"; 
 char* msg3 = "hello, world #3"; 
 char* msg4 = "hello, world #4"; 
+int a=10,b=40,c=30;
 
   
 int main() 
@@ -16,18 +17,19 @@ int main()
         exit(1); 
   
     /* continued */
-    /* write pipe */
+    /* write pipe 
   
     write(p[1], msg1, 16); 
     write(p[1], msg2, 16); 
     write(p[1], msg3, 16);
-   write(p[1], msg4, 16);
+   write(p[1], msg4, 16);*/
+  write(p[1], a, (sizeof(int)));
     
   
-    for (i = 0; i < 4; i++) { 
+    //for (i = 0; i < 1; i++) { 
         /* read pipe */
-        read(p[0], inbuf, 16); 
+        read(p[0], inbuf, (sizeof(int))); 
         printf("% s\n", inbuf); 
-    } 
+    //} 
     return 0; 
 } 
