2a372cda4775880f86bb6d419c4769d99aa5af2f
ajay16108 <65584647+ajay16108@users.noreply.github.com>
2021-02-23 13:45:11 +0530

    Update seekread.c
    dsdsd

--- seekread.c
+++ seekread.c
@@ -10,7 +10,7 @@ int main()
     ll a2;
     FILE *fp;
     fp=fopen("data","rb");
-    fseek(fp,(2*(sizeof(a2))),SEEK_SET);
+    //fseek(fp,(2*(sizeof(a2))),SEEK_SET);
     fread(&a2,(sizeof(a2)),1,fp);
     printf("reading done");
     printf("%d %d",a2.data,a2.link);
