99b23d1fa39128c8b9d46cffdd013426b4216c01
ajay16108 <65584647+ajay16108@users.noreply.github.com>
2021-02-24 16:39:38 +0530

    Update pipeforkint.c
    eded

--- pipeforkint.c
+++ pipeforkint.c
@@ -27,9 +27,9 @@ if((pid = fork()) < 0){
 if(pid>0){
     int arr1=0;
     ppid=getpid();
-    write(fd[1], a,sizeof(int));
+    write(fd[1], &a,sizeof(int));
     sleep(5);
-    read(fd1[0],arr1,sizeof(int));
+    read(fd1[0],&arr1,sizeof(int));
     printf("%d data is %d\n",ppid,arr1);
     exit(0);
 
@@ -40,9 +40,9 @@ else{
     close(fd[1]);
     pid1=getpid();
     sleep(5);
-    read(fd[0], arr, sizeof(int));
+    read(fd[0], &arr, sizeof(int));
     printf("%d data is %d\n",pid1,arr);
-    write(fd1[1],b,sizeof(int));
+    write(fd1[1],&b,sizeof(int));
     return 0;
 }
 }
