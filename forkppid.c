126053b98785d2342e768b1d625481e8f61828f5
ajay16108 <65584647+ajay16108@users.noreply.github.com>
2021-02-24 15:21:50 +0530

    Update forkppid.c
    dd

--- forkppid.c
+++ forkppid.c
@@ -1,31 +1,47 @@
 #include <stdio.h>
-#include <sys/types.h>
 #include <unistd.h>
-
+#include <stdlib.h>
 int main() {
-   pid_t pid, mypid, myppid;
-   pid = getpid();
-   printf("Before fork: Process id is %d\n", pid);
-   pid = fork();
+int fd[2];
+int pid1;
+int ppid;
+int fd1[2];
+int pid;
 
-   if (pid < 0) {
-      perror("fork() failure\n");
-      return 1;
-   }
+if(pipe(fd) == -1){
+    perror("pipe");
+    exit(1);
+}
+if(pipe(fd1) == -1){
+    perror("pipe");
+    exit(1);
+}
 
-   // Child process
-   if (pid == 0) {
-      printf("This is child process\n");
-      mypid = getpid();
-      myppid = getppid();
-      printf("Process id is %d and PPID is %d\n", mypid, myppid);
-   } else { // Parent process 
-      sleep(2);
-      printf("This is parent process\n");
-      mypid = getpid();
-      myppid = getppid();
-      printf("Process id is %d and PPID is %d\n", mypid, myppid);
-      printf("Newly created process id or child pid is %d\n", pid);
-   }
-   return 0;
+
+if((pid = fork()) < 0){
+    perror("fork");
+    exit(1);
+}
+
+if(pid>0){
+    int arr1[10];
+    ppid=getpid();
+    write(fd[1], "hello", 20);
+    sleep(10);
+    read(fd1[0],arr1,20);
+    printf("%d data is %s\n",ppid,arr1);
+    exit(0);
+
+}
+else{
+   
+    char arr[10];
+    close(fd[1]);
+    pid1=getpid();
+    sleep(10);
+    read(fd[0], arr, 10);
+    printf("%d data is %s\n",pid1,arr);
+    write(fd1[1],"HELLO",10);
+    return 0;
+}
 }
