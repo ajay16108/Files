5654fec553452d7837aa0f9b553b8451cc5d4767
ajay16108 <65584647+ajay16108@users.noreply.github.com>
2021-02-25 12:31:12 +0530

    Update deleteemp.c

--- deleteemp.c
+++ deleteemp.c
@@ -12,8 +12,8 @@ int main()
     fp=fopen("employeedb","wb");
     printf("enter employee id to search");
     scanf("%d",&id);
- //while((fread(&obj2[i],sizeof(obj2),1,fp) ==1))
- while(employeedb!=NULL)
+ while((fread(&obj2[i],sizeof(obj2),1,fp) ==1))
+ //while(employeedb!=NULL)
  {
     for(i=0;i<=3;i++)
     {
