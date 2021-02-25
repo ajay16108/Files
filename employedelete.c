58682f88d3375b963216b40481045c73ea212c89
ajay16108 <65584647+ajay16108@users.noreply.github.com>
2021-02-25 15:48:03 +0530

    Update employedelete.c

--- employedelete.c
+++ employedelete.c
@@ -9,23 +9,22 @@ struct employee obj2[10];
 int main()
 {    int id;;int i;int index=0;
     FILE *fp,*fp1;
-    fp=fopen("employeedb","rb");
-    fp1=fopen("employee","wb");
+    fp=fopen("employeedb","r+b");
+   // fp1=fopen("employee","wb");
     printf("enter employee id to search");
     scanf("%d",&id);
     while((fread(&obj2[i],sizeof(obj2),1,fp) ==1))
     {
         for(i=0;i<=3;i++)
         {
-            if (obj2[i].empID!= id)
+            if (obj2[i].empID== id)
             {
-                fwrite(&obj2[i], sizeof(obj2), 1, fp1);
+                print("Enter the new data ");
+                scanf("%d %s %s", &obj2[i].empID,&obj2[i].empname,&obj3[i].company);
+                fwrite(&obj2[i], sizeof(obj2), 1, fp);
             } 
         }
     }
     fclose(fp);
-    fclose(fp1);
-    remove("employeedb");
-    rename("employee", "employeedb");
-    printf("\nRecord deleted.");
+    printf("\nRecord updated.");
 }
