4382aa9e006bc4596c89d2921504b7336b9ccefc
ajay16108 <65584647+ajay16108@users.noreply.github.com>
2021-02-25 16:01:11 +0530

    Create updateemployee.c
    sdd

---
+++ updateemployee.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+struct employee
+{
+    int empID;
+    char empname[20];
+    char company[20];
+}obj1[10];
+struct employee obj2[10];
+int main()
+{    int id;;int i;int index=0;
+    FILE *fp,*fp1;
+    fp=fopen("employeedb","r+b");
+    printf("enter employee id to update");
+    scanf("%d",&id);
+    while((fread(&obj2[i],sizeof(obj2),1,fp) ==1))
+    {
+        for(i=0;i<=3;i++)
+        {
+            if (obj2[i].empID == id)
+            {
+                printf("Enter the new data ");
+                scanf("%d %s %s", &obj2[i].empID,&obj2[i].empname,&obj2[i].company);
+                fwrite(&obj2[i], sizeof(obj2), 1, fp);
+            } 
+        }
+    }
+    fclose(fp);
+    printf("\nRecord updated.");
+}
