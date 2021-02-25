903c0c84ad628774029521bc555c2f58f763aa02
ajay16108 <65584647+ajay16108@users.noreply.github.com>
2021-02-25 11:49:44 +0530

    Update employeesearch.c

--- employeesearch.c
+++ employeesearch.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 struct employee
 {
@@ -6,9 +5,9 @@ struct employee
     char empname[20];
     char company[20];
 }obj1[10];
-struct employee obj2[10];
+//struct employee obj2[10];
 int main()
-{    int id;int flag;int i;
+{    int id;int flag;int i;int index;
     FILE *fp;
     fp=fopen("employeedb","rb");
     printf("enter employee id to search");
@@ -18,15 +17,20 @@ int main()
        if(obj1[i].empID==id)
        {
            flag=1;
+           index=i;
            break;
        }
     }
     if(flag==1)
     {
         printf("found");
-        printf("%s %s",obj1[i].empname,obj1[i].company);
+        printf("%s %s",obj1[index].empname,obj1[index].company);
     }
-    
+ if(flag==0)
+ {
+     printf("not found");
+ }
    fclose(fp);
 }
+
 
