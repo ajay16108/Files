9c54febaf9dea0ae4ad69ee0d52222fb9d28df5a
ajay16108 <65584647+ajay16108@users.noreply.github.com>
2021-02-25 11:00:05 +0530

    Create reademployee.c
    sdsad

---
+++ reademployee.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+struct employee
+{
+    int empID;
+    char empname[20];
+    char company[20];
+}obj1[10];
+struct employee obj2[10];
+int main()
+{
+    FILE *fp;
+    fp=fopen("employeedb","rb");
+    for(int i=0;i<3;i++)
+    {
+        fread(&obj2[i],sizeof(obj2),1,fp);
+    }
+    for(int i=0;i<3;i++)
+    {
+        printf("%d %s %s",obj2[i].empID,obj2[i].empname,obj2[i].company);
+    }
+    
+    fclose(fp);
+}
