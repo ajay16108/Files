6dbfef03b2785c3919629b55f48aeca5cbc39377
ajay16108 <65584647+ajay16108@users.noreply.github.com>
2021-02-25 11:53:29 +0530

    Update employeesearch.c

--- employeesearch.c
+++ employeesearch.c
@@ -5,26 +5,29 @@ struct employee
     char empname[20];
     char company[20];
 }obj1[10];
-//struct employee obj2[10];
+struct employee obj2[10];
 int main()
 {    int id;int flag;int i;int index;
     FILE *fp;
     fp=fopen("employeedb","rb");
     printf("enter employee id to search");
     scanf("%d",&id);
+ while(fread(&obj2[i],sizeof(obj2),1,fp)==1)
+ {
     for(i=0;i<3;i++)
     {
-       if(obj1[i].empID==id)
+       if(obj2[i].empID==id)
        {
            flag=1;
            index=i;
            break;
        }
     }
+ }
     if(flag==1)
     {
         printf("found");
-        printf("%s %s",obj1[index].empname,obj1[index].company);
+        printf("%s %s",obj2[index].empname,obj2[index].company);
     }
  if(flag==0)
  {
